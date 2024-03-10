bd2d49132f411cde9263d6e8614d8ec55c7fa36e
Михаил Пузицкий <gretawolf@live.ru>
2024-03-10 18:55:47 +0300

    Add files via upload

--- NLP_generation/Generation/utils/telegram_genbot.py
+++ NLP_generation/Generation/utils/telegram_genbot.py
@@ -22,31 +22,97 @@ from utils.download import gd_folder_download
 from utils.constant import SHARE_DIR, TOKEN
 from utils.generate import generate_answer, get_prompt, ROLES
 
+with open('/content/model_dir.txt') as f:
+    model_dir = f.read()
 
-# Подгружаем с публичной папки по https
-dir_to = MAIN_DIR + "/TinyLamma/"
 
 def main():
 
     print('Please wait messsage - READY\n')
     print('Load folder with weights and model and tokenizer\n')
-    # код потребует гугл авторизацию любую из имеющихся
-    model_dir = gd_folder_download(SHARE_DIR, dir_to)
 
     model = AutoModelForCausalLM.from_pretrained(model_dir)
     model.to(DEVICE)
 
     # Loading the model with double quantization
-    model_name =model.config._name_or_path
+    model_name = model.config._name_or_path
     # Creating tokenizer and defining the pad token
     tokenizer = AutoTokenizer.from_pretrained(model_name, 
                                             trust_remote_code=True, 
                                             padding_side='right')
+                                            
     tokenizer.pad_token = tokenizer.eos_token
 
 
     # Creating tokenizer and definin
     print('Model loaded\n')
+
+
+    # Создаем экземпляр бота
+    bot = telebot.TeleBot(TOKEN)
+
+    # Функция, обрабатывающая команду /start
+    @bot.message_handler(commands=["start"])
+    def start(m, res=False):
+        # Добавляем две кнопки
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        for role in ROLES:
+            item=types.KeyboardButton(role)
+            markup.add(item)
+
+        bot.send_message(m.chat.id, 'Бот запущен. Начните общение')
+
+    conext_memory = ''
+    get_role = False
+
+    # Получение сообщений от пользователя
+    @bot.message_handler(content_types=["text"])
+    def handle_text(message):
+        global conext_memory, get_role
+
+        #print("in_conext_memory", conext_memory)
+        #print()
+
+        if not get_role:
+            for role in ROLES:
+                #print("message.text", message.text)
+                #print(role)
+                if role in message.text:
+                    #print("get_role ", role)
+                    get_role = role
+                    break
+
+            if not get_role: get_role = "Kyle"
+
+        if get_role:
+            prompt = get_prompt(query = message.text,
+                                context = conext_memory,
+                                role = get_role)
+            #print("have role" , get_role)
+
+
+        gen_answer = generate_answer(prompt, model, tokenizer, 
+                                     role = get_role,
+                                     max_new_tokens = 250,
+                                     device = DEVICE)
+
+        conext_memory = '.'.join([conext_memory, message.text, gen_answer])
+
+        #print("gen_answer", gen_answer)
+        #print()
+        #print("out_conext_memory", conext_memory)
+        # Отсылаем сообщение в чат пользователя
+        try:
+          bot.send_message(message.chat.id, gen_answer)
+        except: pass
+
+    # Запускаем бота
+    print('READY  to talk in - https://t.me/SouthPark_gen_test_bot\n')
+    bot.polling(none_stop=True, interval=0)
+
+
+if __name__ == "__main__":
+    main()
 
 
     # Создаем экземпляр бота

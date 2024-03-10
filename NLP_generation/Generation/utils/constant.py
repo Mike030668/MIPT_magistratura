5f7262d8a972a52c176e89f9d1a55b2581d4570b
Михаил Пузицкий <gretawolf@live.ru>
2024-03-10 16:05:26 +0300

    Add files via upload

--- NLP_generation/Generation/utils/constant.py
+++ NLP_generation/Generation/utils/constant.py
@@ -1,6 +1,7 @@
 # Подгружаем с публичной папки по https
 #SHARE_DIR = 'https://drive.google.com/drive/folders/1IF_fNTyjfDQCazHNfaY8YP3uuLLzTFCK?usp=sharing'
-SHARE_DIR =  "https://drive.google.com/drive/folders/12Y3D_T4uT-T4PJ7cn48jmdAd3r-MX85M?usp=sharing"
+#SHARE_DIR =  "https://drive.google.com/drive/folders/12Y3D_T4uT-T4PJ7cn48jmdAd3r-MX85M?usp=sharing"
+SHARE_DIR =  "https://drive.google.com/drive/folders/14XOcfs33nDjMcgTArYgb5Qu2t-p17KYn?usp=sharing"
 
 TOKEN = "6667197552:AAH-1-Jk2FAoY3YxPrIbMCwrinXpMsJWe7o"
 
